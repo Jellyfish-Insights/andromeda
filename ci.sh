aad970b859748a04d456f0696381e677c887ff3e
Lucas Lovato <lucas.lovato@gmail.com>
2020-01-13 11:09:47 -0300

    Add Continuous Integration (#13)
    * add CI
    
    * update dotnet test
    
    * delete uncrustify

--- ci.sh
+++ ci.sh
@@ -2,47 +2,37 @@
 
 set -ex
 
+# link the hostnames data_lake and analytics_platform to the localhost address
+sudo /bin/bash -c 'echo -e "127.0.0.1 data_lake" >> /etc/hosts'
+sudo /bin/bash -c 'echo -e "127.0.0.1 analytics_platform" >> /etc/hosts'
+
 # check versions
 dotnet --info
 node --version
 npm --version
-tsfmt --version
-uncrustify --version
+docker --version
+docker-compose --version
 
-# check for commited password
-test $(grep 'Password=' $(find . -name 'appsettings.json') | grep -v 'Password=dbpassword' | wc -l) -eq 0
 
 # enter source directory
 cd src
 
-# check C# style
-uncrustify -c ../uncrustify.cfg --check $(find . -name '*.cs' | grep -v "Migrations")
+# up databases
+docker-compose -f docker-compose.test.yml up -d
+
+# install the correct node version
+sudo npm install n
+sudo n 9.11.1
 
 # enter WebApp directory
 cd WebApp
-
-# check TS(X) style
-tsfmt --verify $(find . -name '*.ts' -o -name '*.tsx' | grep -v 'types.*\.ts$')
-
-# install front-end dependencies
 npm install
-
-# run front-end tests
 npm test
 
-# go back to source directory
 cd ..
-
 # build all projects
-dotnet build
+dotnet build ./ap.sln -c Release
 
-# fix connection strings
-./use-ci-conn-string
-
-# apply migrations
-cd ConsoleApp
-./migrate.sh
-
-# run C# tests
-cd ../Test
-dotnet test --verbosity normal --no-restore --no-build
+# run c# tests
+cd Test
+dotnet test --verbosity normal
