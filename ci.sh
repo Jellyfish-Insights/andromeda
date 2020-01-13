120e081cb46747c1de7ad8b241d2741173169cf1
Lucas Lovato <lucaslovato@brickabode.com>
2020-01-13 10:05:43 -0300

    delete uncrustify

--- ci.sh
+++ ci.sh
@@ -6,23 +6,13 @@ set -ex
 sudo /bin/bash -c 'echo -e "127.0.0.1 data_lake" >> /etc/hosts'
 sudo /bin/bash -c 'echo -e "127.0.0.1 analytics_platform" >> /etc/hosts'
 
-# Installing uncrustify
-RUN git clone https://github.com/uncrustify/uncrustify.git \
-    && cd uncrustify && git checkout "uncrustify-0.67" \
-    && mkdir build && cd build \
-    && cmake .. && make && make install \
-    && cd ../../ && rm -rf uncrustify
-
 # check versions
 dotnet --info
 node --version
 npm --version
 docker --version
 docker-compose --version
-uncrustify --version
 
-# Checking if the code is formated
-uncrustify -c ../uncrustify.cfg --check $(find . -name '*.cs' | grep -v "Migrations")
 
 # enter source directory
 cd src
