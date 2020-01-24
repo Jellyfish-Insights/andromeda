0a5ff45d9988476e007819aff35e0b25647a84a2
Lucas Lovato <lucaslovato@brickabode.com>
2020-01-24 15:08:25 -0300

    Add uncrustify on the ci

--- ci.sh
+++ ci.sh
@@ -6,13 +6,28 @@ set -ex
 sudo /bin/bash -c 'echo -e "127.0.0.1 data_lake" >> /etc/hosts'
 sudo /bin/bash -c 'echo -e "127.0.0.1 analytics_platform" >> /etc/hosts'
 
+sudo apt-get install build-essential
+
+git clone https://github.com/uncrustify/uncrustify.git \
+    && cd uncrustify && git checkout "uncrustify-0.67" \
+    && mkdir build && cd build \
+    && cmake .. && make && make install \
+    && cd ../../ && rm -rf uncrustify
+
 # check versions
 dotnet --info
 docker --version
 docker-compose --version
+uncrustify --version
 
 # enter source directory
 cd src
+
+#Uncrustify verification
+uncrustify -c ../uncrustify.cfg --check $(find . -name '*.cs' | grep -v "Migrations")
+
+#If check is failing run this command locally
+#uncrustify -c ../uncrustify.cfg --no-backup $(find . -name '*.cs' | grep -v "Migrations")
 
 # up databases
 docker-compose -f docker-compose.test.yml up -d
