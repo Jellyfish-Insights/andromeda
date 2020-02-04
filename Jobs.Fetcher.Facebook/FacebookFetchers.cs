856cb803718b982f12515668a99f61fa814f3508
Leonardo Viana <lvt@brickabode.com>
2020-02-04 12:44:17 -0300

    Handle some credential's exceptions

--- Jobs.Fetcher.Facebook/FacebookFetchers.cs
+++ Jobs.Fetcher.Facebook/FacebookFetchers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using Andromeda.Common.Jobs;
 using Serilog.Core;
+using System.IO;
 
 namespace Jobs.Fetcher.Facebook {
 
@@ -28,29 +29,13 @@ namespace Jobs.Fetcher.Facebook {
 
             var jobs = new List<FacebookFetcher>();
 
-            foreach (var schemaName in Schemas) {
+            try
+            {
+                foreach (var schemaName in Schemas) {
 
-                var schema = SchemaLoader.LoadSchema(schemaName);
-                var apiMan = new ApiManager(
-                    SchemaLoader.GetCredentials(schemaName),
-                    IgnoreCache,
-                    jobConfiguration.IgnoreAPI,
-                    jobConfiguration.IgnoreTTL,
-                    jobConfiguration.Paginate,
-                    schema.Delay,
-                    CacheDirectory,
-                    jobConfiguration.DefaultNowDate
-                    );
-                var fetcher = new Fetcher(apiMan, schema.PageSize);
-                var customEdge = new CustomEdge();
-
-                if (schemaName == "adaccount") {
-
-                    var pageSchema = SchemaLoader.LoadSchema("page");
-                    var pageCredentials = SchemaLoader.GetCredentials("page");
-
-                    var pageApiMan = new ApiManager(
-                        SchemaLoader.GetCredentials("page"),
+                    var schema = SchemaLoader.LoadSchema(schemaName);
+                    var apiMan = new ApiManager(
+                        SchemaLoader.GetCredentials(schemaName),
                         IgnoreCache,
                         jobConfiguration.IgnoreAPI,
                         jobConfiguration.IgnoreTTL,
@@ -59,48 +44,76 @@ namespace Jobs.Fetcher.Facebook {
                         CacheDirectory,
                         jobConfiguration.DefaultNowDate
                         );
-                    var pageFetcher = new Fetcher(pageApiMan, pageSchema.PageSize);
+                    var fetcher = new Fetcher(apiMan, schema.PageSize);
+                    var customEdge = new CustomEdge();
 
-                    var key = (SchemaName : "adaccount", TableName : "ads", CustomEdgeName : "custom_videos");
-                    if (!customEdge.ContainsKey(key)) {
+                    if (schemaName == "adaccount") {
 
-                        Action<Logger, JObject> callback = (logger, parent) => {
-                            var creative = parent["creative"];
+                        var pageSchema = SchemaLoader.LoadSchema("page");
+                        var pageCredentials = SchemaLoader.GetCredentials("page");
 
-                            var video = creative ? ["video_id"];
-                            if (video != null) {
-                                pageFetcher.GetRoot(pageSchema, "videos", video, logger);
-                            }
+                        var pageApiMan = new ApiManager(
+                            SchemaLoader.GetCredentials("page"),
+                            IgnoreCache,
+                            jobConfiguration.IgnoreAPI,
+                            jobConfiguration.IgnoreTTL,
+                            jobConfiguration.Paginate,
+                            schema.Delay,
+                            CacheDirectory,
+                            jobConfiguration.DefaultNowDate
+                            );
+                        var pageFetcher = new Fetcher(pageApiMan, pageSchema.PageSize);
 
-                            var sourceFileVideoId = creative.IndexPathOrDefault<string>("object_story_spec.video_data.video_id", null);
+                        var key = (SchemaName : "adaccount", TableName : "ads", CustomEdgeName : "custom_videos");
+                        if (!customEdge.ContainsKey(key)) {
 
-                            if (sourceFileVideoId != null) {
-                                pageFetcher.GetRoot(pageSchema, "videos", sourceFileVideoId, logger);
-                            }
+                            Action<Logger, JObject> callback = (logger, parent) => {
+                                var creative = parent["creative"];
 
-                            var post = creative ? ["object_story_id"];
-                            if (post != null) {
-                                pageFetcher.GetRoot(pageSchema, "posts", post, logger);
-                            }
-                        };
+                                var video = creative ? ["video_id"];
+                                if (video != null) {
+                                    pageFetcher.GetRoot(pageSchema, "videos", video, logger);
+                                }
 
-                        customEdge.Add(key, callback);
+                                var sourceFileVideoId = creative.IndexPathOrDefault<string>("object_story_spec.video_data.video_id", null);
+
+                                if (sourceFileVideoId != null) {
+                                    pageFetcher.GetRoot(pageSchema, "videos", sourceFileVideoId, logger);
+                                }
+
+                                var post = creative ? ["object_story_id"];
+                                if (post != null) {
+                                    pageFetcher.GetRoot(pageSchema, "posts", post, logger);
+                                }
+                            };
+
+                            customEdge.Add(key, callback);
+                        }
                     }
-                }
 
-                if (Schemas.Count == 1 && TableKey != null) {
-                    // DEPRECATED: case in which the user specified a single edge to be executed
-                    jobs.Add(new FacebookFetcher(schema, TableKey, EdgeKey, fetcher, customEdge));
-                } else {
-                    foreach (var edge in schema.Edges) {
-                        jobs.Add(new FacebookFetcher(schema, edge.Key, null, fetcher, customEdge));
-                    }
-                    if (schemaName == "instagram") {
-                        jobs.Add(new FacebookFetcher(schema, null, null, fetcher, customEdge));
+                    if (Schemas.Count == 1 && TableKey != null) {
+                        // DEPRECATED: case in which the user specified a single edge to be executed
+                        jobs.Add(new FacebookFetcher(schema, TableKey, EdgeKey, fetcher, customEdge));
+                    } else {
+                        foreach (var edge in schema.Edges) {
+                            jobs.Add(new FacebookFetcher(schema, edge.Key, null, fetcher, customEdge));
+                        }
+                        if (schemaName == "instagram") {
+                            jobs.Add(new FacebookFetcher(schema, null, null, fetcher, customEdge));
+                        }
                     }
                 }
             }
-
+            catch (Exception e) when (e is FileNotFoundException || e is DirectoryNotFoundException)
+            {
+                string message = String.Format("\nMissing or invalid Facebook credentials!\n{0}", e.Message);
+                if (e is DirectoryNotFoundException) {
+                    message = String.Format("{0}\nCheck if the path above exists!", message);
+                }
+                System.Console.WriteLine(message);
+                Environment.Exit(1);
+            };
+            
             return FilterByName(jobs, names);
         }
     }
