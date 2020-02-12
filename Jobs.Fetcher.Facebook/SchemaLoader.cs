a2036b351cca962c802419d479eb23e209c83df8
Leonardo Viana <53630700+leonardo-brickabode@users.noreply.github.com>
2020-02-12 09:14:12 -0300

    Improve missing credential messages (#61)

--- Jobs.Fetcher.Facebook/SchemaLoader.cs
+++ Jobs.Fetcher.Facebook/SchemaLoader.cs
@@ -22,15 +22,37 @@ namespace Jobs.Fetcher.Facebook {
             var all_schemas = new List<string> { "page", "adaccount", "instagram" };
             var valid_schemas = new List<string>();
             foreach(var schema_name in all_schemas){
-                if(File.Exists($"credentials/facebook/" + schema_name + "_credentials.json")) {
+                if(File.Exists(GetSchemaPath(schema_name))) {
                     valid_schemas.Add(schema_name);
+                } else { 
+                    System.Console.WriteLine("Failed to get " + GetServiceName(schema_name) + " data!");
+                    System.Console.WriteLine("Couldn't find file \'" + GetSchemaPath(schema_name) + "\'");
                 }
             }
             return valid_schemas;
         }
 
+        private static string GetServiceName(string schema_name) {
+            switch (schema_name) {
+                case "page":
+                    return "Facebook";
+                case "adaccount":
+                    return "Facebook Marketing";
+                case "instagram":
+                    return "Instagram";
+                default:
+                    return "Undefined";
+            }
+        }
+
+        private static string GetSchemaPath(string schema_name) {
+            var pre_path = schema_name == "instagram" ? "credentials/instagram/" : "credentials/facebook/";
+            return pre_path + schema_name + "_credentials.json";
+        }
+
+
         public static T ParseCredentials<T>(string schema_name) {
-            string contents = File.ReadAllText("credentials/facebook/" + schema_name + "_credentials.json");
+            string contents = File.ReadAllText(GetSchemaPath(schema_name));
             return JsonConvert.DeserializeObject<T>(contents);
         }
 
