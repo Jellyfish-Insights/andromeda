5175df5a30e2ea11e8eef5871fd6bec586af77ea
Eric Shimizu Karbstein <90923453+EricShimizuKarbstein@users.noreply.github.com>
2021-09-21 17:07:59 -0300

    Feature: multiple user folders (#108)
    * Update the Facebook fetcher to handle the new credentials structure
    
    * Update YouTube fetcher to handle new credential folder structure
    
    * Detect and handle case of old folder path for Youtube and Partially do the same for Facebook
    
    * Better detection of old folder structure

--- Jobs.Fetcher.Facebook/SchemaLoader.cs
+++ Jobs.Fetcher.Facebook/SchemaLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using DataLakeModels;
@@ -19,11 +20,36 @@ namespace Jobs.Fetcher.Facebook {
 
         public static List<string> SchemaList() {
             var allSchemas = new List<string> { "page", "adaccount", "instagram" };
-            return CheckCredentialStatus(allSchemas);
+            var validSchemas = CheckCredentialStatus(allSchemas);
+
+            // First check for the old folder structure
+            if (validSchemas.Count > 0) {
+                Console.WriteLine($"Detected old folder structure. Loading only the old structure schemas. Please, consider changing to the new folder structure");
+                return validSchemas;
+            }
+
+            var setOfSchemas = new HashSet<string>();
+            var userDirs = new List<string>(Directory.GetDirectories("./credentials"));
+            foreach(var userDir in userDirs) {
+                validSchemas = CheckCredentialStatus(allSchemas, userDir);
+                foreach(var schema in validSchemas) {
+                    setOfSchemas.Add(schema);
+                }
+            }
+            return new List<string>(setOfSchemas);
+        }
+
+        public static List<string> SchemaList(string usrFolderName) {
+            var allSchemas = new List<string> { "page", "adaccount", "instagram" };
+            return CheckCredentialStatus(allSchemas, usrFolderName);
         }
 
         public static List<string> SchemaList(List<string> allSchemas) {
             return CheckCredentialStatus(allSchemas);
+        }
+
+        public static List<string> SchemaList(List<string> allSchemas, string usrFolderName) {
+            return CheckCredentialStatus(allSchemas, usrFolderName);
         }
 
         private static List<string> CheckCredentialStatus(List<string> allSchemas) {
@@ -33,8 +59,23 @@ namespace Jobs.Fetcher.Facebook {
                 if (Directory.Exists(credentialPath) && Directory.GetFiles(credentialPath).Length > 0) {
                     validSchemas.Add(schemaName);
                 } else {
-                    System.Console.WriteLine($"Missing or invalid {GetServiceName(schemaName)} credentials!");
-                    System.Console.WriteLine($"Couldn't find any credential on folder '{credentialPath}'");
+                    Console.WriteLine($"Missing or invalid {GetServiceName(schemaName)} credentials!");
+                    Console.WriteLine($"Couldn't find any credential on folder '{credentialPath}'");
+                }
+            }
+            return validSchemas;
+        }
+
+        private static List<string> CheckCredentialStatus(List<string> allSchemas, string usrFolderName) {
+            var validSchemas = new List<string>();
+            foreach (var schemaName in allSchemas) {
+                var credentialPath = GetCredentialPath(schemaName, usrFolderName);
+                if (Directory.Exists(credentialPath) && Directory.GetFiles(credentialPath).Length > 0) {
+                    validSchemas.Add(schemaName);
+                } else {
+                    Console.WriteLine($"Missing or invalid {GetServiceName(schemaName)} credentials!");
+                    Console.WriteLine($"Couldn't find any credential on folder '{credentialPath}'");
+                    Console.WriteLine($"File: {credentialFileName}\n");
                 }
             }
             return validSchemas;
@@ -54,8 +95,29 @@ namespace Jobs.Fetcher.Facebook {
         }
 
         public static string GetCredentialPath(string schemaName) {
-            var prePath = schemaName == "instagram" ? "credentials" : "credentials/facebook";
-            return $"{prePath}/{schemaName}";
+            switch (schemaName) {
+                case "instagram":
+                    return "./credentials/instagram";
+                case "page":
+                    return $"./credentials/facebook/{schemaName}";
+                case "adaccount":
+                    return $"./credentials/facebook/{schemaName}";
+                default:
+                    return "./credentials/";
+            }
+        }
+
+        public static string GetCredentialPath(string schemaName, string usrFolder) {
+            switch (schemaName) {
+                case "instagram":
+                    return $"{usrFolder}/instagram";
+                case "page":
+                    return $"{usrFolder}/facebook/{schemaName}";
+                case "adaccount":
+                    return $"{usrFolder}/facebook/{schemaName}";
+                default:
+                    return usrFolder;
+            }
         }
 
         public static T ParseCredentials<T>(string schemaName) {
