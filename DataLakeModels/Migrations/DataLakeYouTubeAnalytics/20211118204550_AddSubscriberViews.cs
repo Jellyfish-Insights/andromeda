a44d4c55499dd5c1a7b6731c3bb5d21ba89c13a7
Marcos Fleury <fleury@brickabode.com>
2021-12-02 17:45:14 -0300

    Adds SubscriberViews to YouTube fetchers and data_lake

---
+++ DataLakeModels/Migrations/DataLakeYouTubeAnalytics/20211118204550_AddSubscriberViews.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLakeModels.Migrations.DataLakeYouTubeAnalytics
+{
+    public partial class AddSubscriberViews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "SubscriberViews",
+                schema: "youtube_analytics_v2",
+                table: "VideoDailyMetrics",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubscriberViews",
+                schema: "youtube_analytics_v2",
+                table: "VideoDailyMetrics");
+        }
+    }
+}
