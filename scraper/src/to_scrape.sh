288efe331d102fbf85bcc55737d8717e679e8d93
Victor Moraes <victor@brickabode.com>
2021-12-24 13:11:41 -0300

    Copying changes that were previously in tracking/prototypes

---
+++ scraper/src/to_scrape.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+python3 scraper.py --scraping_interval 60 --timeout 45 @feeonline TikTok
+sleep 60
+python3 scraper.py --scraping_interval 60 --timeout 45 @happydogs2021 TikTok
+sleep 60
+python3 scraper.py --scraping_interval 60 --timeout 45 @tiktok TikTok
+sleep 60
