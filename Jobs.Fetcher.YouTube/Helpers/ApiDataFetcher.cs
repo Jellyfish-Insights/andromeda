a3354451d84d10ce2d6e9795105ee1ffd2ea6265
MarcosFleury <90846556+MarcosFleury@users.noreply.github.com>
2022-01-27 14:23:22 -0300

    Improves fetching speed (#158)

--- Jobs.Fetcher.YouTube/Helpers/ApiDataFetcher.cs
+++ Jobs.Fetcher.YouTube/Helpers/ApiDataFetcher.cs
@@ -159,7 +159,7 @@ namespace Jobs.Fetcher.YouTube.Helpers {
             reportRequest.Dimensions = "day";
             reportRequest.Sort = "day";
 
-            Thread.Sleep(500);
+            Thread.Sleep(100);
             var report = reportRequest.ExecuteAsync().Result;
 
             if (report.Rows != null) {
@@ -197,7 +197,7 @@ namespace Jobs.Fetcher.YouTube.Helpers {
 
             var subscriberViewsList = new List<(DateTime date, long subscriberViews)>();
 
-            Thread.Sleep(500);
+            Thread.Sleep(100);
             try {
                 var report = reportRequest.ExecuteAsync().Result;
                 if (report.Rows != null) {
@@ -207,9 +207,8 @@ namespace Jobs.Fetcher.YouTube.Helpers {
                 }
             }
             catch {
-                //logger.Information("Could not get Subscriber Views");
+                logger.Error($"Could not get Subscriber Views for video {video.VideoId}");
             }
-
             return subscriberViewsList;
         }
 
