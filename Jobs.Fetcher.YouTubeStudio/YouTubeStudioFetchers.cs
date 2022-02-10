15bea42fee1c6e953383e21b5829961cafe3f9b9
MarcosFleury <90846556+MarcosFleury@users.noreply.github.com>
2022-02-10 11:57:37 -0300

    Hotfix/facebook fetchers exceptions (#172)
    * Corrects implicit conversion error
    
    * Catches errors on Viewer Percentage Reports and better log them
    
    * Improves Instagram fetcher error handling

--- Jobs.Fetcher.YouTubeStudio/YouTubeStudioFetchers.cs
+++ Jobs.Fetcher.YouTubeStudio/YouTubeStudioFetchers.cs
@@ -1,19 +1,16 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
-
 using DataLakeModels.Models.YouTube.Studio;
 using Andromeda.Common.Jobs;
 
 namespace Jobs.Fetcher.YouTubeStudio {
     public sealed class YouTubeStudioFetchers : FetcherJobsFactory {
         public override JobScope Scope { get; } = JobScope.YouTubeStudio;
-        public override IEnumerable<AbstractJob> GetJobs(
-            JobType type, JobScope scope, IEnumerable<string> names, JobConfiguration config
-            ) {
+        public override IEnumerable<AbstractJob> GetJobs(JobType type, JobScope scope, IEnumerable<string> names, JobConfiguration config) {
             if (CheckTypeAndScope(type, scope) || !CheckNameIsScope(names)) {
                 return NoJobs;
             }
