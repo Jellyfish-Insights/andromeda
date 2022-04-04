6637659df1a46d95b76731c44d1eef5f14357704
Marcos Fleury <fleury@brickabode.com>
2022-04-04 10:50:59 -0300

    Adds missing imports

--- Jobs.Fetcher.Twitter/Jobs/Ads/CustomAudiencesQuery.cs
+++ Jobs.Fetcher.Twitter/Jobs/Ads/CustomAudiencesQuery.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading;
 using Tweetinvi;
 using DataLakeModels;
 using Tweetinvi.Models.V2;
